2156fe445a74a30fe59b567b6457b01701fa7624
LuzKary1613 <137965602+LuzKary1613@users.noreply.github.com>
2023-09-13 18:13:14 -0600

    Update Pregunta8.py

--- Pregunta8.py
+++ Pregunta8.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def func_test(N):
+def function_valoresperado(N):
     R = np.random.uniform(0, 1, N)
     x = (3 ** R) / 2
     return x
 
 N = 100000
-x = func_test(N)
+x = function_valoresperado(N)
 C = 500
 
 plt.hist(x, bins=C)
 plt.show()
 
-print(f'El valor aproximado es de {np.mean(x)}')
+print(f'Valor aproximado: {np.mean(x)}')
